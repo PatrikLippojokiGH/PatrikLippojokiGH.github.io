2e0085131588cd80ed3c9fdaad7472171d458063
PatrikLippojokiGH <patu_lippojoki@hotmail.com>
2024-03-05 13:29:25 +0200

    hienosäätöä

--- game.js
+++ game.js
@@ -1,27 +1,29 @@
 // Phaser scene configia varten
 gameScene = new Phaser.Scene('gameScene')
 
+// Asetetaan tarvittavat alkuarvot
 gameScene.init = function() {
     gameover = false
     speedUp = -200
     start = false
     points = 0
-    pos = [500,550,600,650,700]
+    pos = [300,350,400,450,500,550,600]
 }
 
 gameScene.preload = function() {
     // ladataan tarvittavat tiedostot
     // this.preload.audio('ääni', 'assets/esim-ääni.wav)
     this.load.image('bg','assets/background-day.png')
-    this.load.image('pipeUp','assets/pipe-green.png')
-    this.load.image('pipeDown','assets/pipe-green-down.png')
+    this.load.image('pipe','assets/pipe-green.png')
+    // this.load.image('pipeDown','assets/pipe-green-down.png') ei tarvittukaan
     this.load.image('platform','assets/base.png')
-    this.load.image('bird0','assets/bluebird-midflap.png')
+    this.load.image('bird0','assets/bluebird-midflap.png') // animaatiota varten useampi kuva
     this.load.image('bird1','assets/bluebird-downflap.png')
     this.load.image('bird2','assets/bluebird-upflap.png')
 }
 
 gameScene.create = function() {
+    // Taustakuvan asetus
     bg = this.add.tileSprite(0,0,800,600,'bg').setOrigin(0)
     bg.setInteractive()
     bg.depth = -2
@@ -29,11 +31,14 @@ gameScene.create = function() {
         start = true
         playGame.visible = false
         if(gameover===false){
+            // Putket liikkeelle
             pipes[0].setVelocityX(speedUp)
             pipes[1].setVelocityX(speedUp)
+            // Lintu nousee klikatessa
             flappyBird.setVelocityY(-300)
             flappyBird.play("flight") // animaatio
         } else {
+            // Jos pelaaja on osunut esteesee, klikkauksesta aloitetaan alusta
             gameover = false
             speedUp = -200
             flappyBird.y = 300
@@ -45,8 +50,12 @@ gameScene.create = function() {
             pipesPassed.setText('Points: '+points)
         }
     })
+
+    // Tekstit pelin aloittamiseksi ja pisteiden näyttämiseksi
     playGame = this.add.text(100,0,'Click\nTo\nStart',{fontSize:'68px'})
     pipesPassed = this.add.text(600,0,'Points: '+points, {fontSize:'20px'})
+
+    // Maahan osumista varten
     ghostplatform = this.physics.add.sprite(800,565,'ghostplatform').setOrigin()
     ghostplatform.setScale(100,0.5)
     ghostplatform.body.allowGravity=false
@@ -73,9 +82,11 @@ gameScene.create = function() {
 
     // Luodaan kaksi putkea sisältävä joukko
     pipe = this.physics.add.group({
-        key:'pipeUp',
+        key:'pipe',
         repeat:1
     })
+
+    // Tehdään kahdesta luodusta putkesta läpi mentävä este
     pipes = pipe.getChildren()
     for(var i=0;i<pipes.length;i++) {
         pipes[i].body.allowGravity=false
@@ -85,7 +96,7 @@ gameScene.create = function() {
         pipes[1].flipY = true
         pipes[i].depth = -1
         pipes[i].setVelocityX(-300)
-        pipes[0].y = pos[Phaser.Math.Between(0,4)]
+        pipes[0].y = pos[Phaser.Math.Between(0,6)]
         pipes[0].x = 800
         pipes[1].y = pipes[0].y-650
         pipes[1].x = pipes[0].x
@@ -98,6 +109,7 @@ gameScene.create = function() {
 
 }
 
+// Tässä olisi ollut mahdollisuus tehdä eri toimenpiteitä kuten ääniä eri osumille, mutta toistaiseksi putki ja maa osumista vain häviää
 gameScene.hitPlatform = function(p1,p2) {
     if(p2===flappyBird) {
         gameover = true
@@ -110,27 +122,33 @@ gameScene.hitPipe = function(p1,p2) {
     }
 }
 
+// Pelin toiminta
 gameScene.update = function() {
+    // Jos ei ole aloitettu, kaikki on paikallaan
     if(start===false) {
         flappyBird.y = 300
         flappyBird.setVelocityY(0)
         pipes[0].setVelocityX(0)
         pipes[1].setVelocityX(0)
     } else {
-
+        // Jos on aloitettu, eikä olla viekä hävitty =>
         if(gameover===false) {
+
+            // Liikutetaan palikoita vasemmalle
             bg.tilePositionX+=0.1
             platform.TilePositionX+=5
-            pipes[1].y = pipes[0].y-650
+            pipes[1].y = pipes[0].y-700
             pipes[1].x = pipes[0].x
 
+            // Jos putket ovat liikkuneet riittävän paljon vasemmalle, luodaan uudet putket ja nopeutetaan peliä
             if(pipes[0].x <= -50) {
-                speedUp -= 50
+                speedUp -= 25
                 pipes[0].x = 850
-                pipes[0].y = pos[Phaser.Math.Between(0,4)]
+                pipes[0].y = pos[Phaser.Math.Between(0,6)]
                 pipes[0].setVelocityX(speedUp)
             }
 
+        // Jos on hävitty, pysäytetään palikat ja käännetään lintua
         } else {
             pipes[0].setVelocityX(0)
             pipes[0].setVelocityY(0)
